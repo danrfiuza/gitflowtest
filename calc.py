5cd3caefccd278a7247d51399abada081ba619c8
danrfiuza <danielfiuza01@gmail.com>
2020-07-09 10:34:00 -0300

    Added sub logic in calc function

--- calc.py
+++ calc.py
@@ -1,6 +1,7 @@
-def calcTwoNumbers(num1, num2, operation):
+def calcTwoNumbers(num1, num2, op):
     result = 0
-    if operation == 'sum':
+    if op == 'sum':
         result = num1 + num2
-
+    elif op == 'sub':
+        result = num1 - num2
     return result
